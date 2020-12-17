f42f964a5c0c58b3c545f1c3a8f15649c7112f38
Alex <alexgarcia1771@outlook.com>
2020-12-17 23:39:08 +0100

    r

--- src/api/service 2.js
+++ src/api/service 2.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Service {
   constructor() {
     this.service = axios.create({
-      baseURL: process.env.REACT_APP_API_URI,
+      baseURL: "http://localhost:4000/api",
       // withCredentials: true
     });
   }
