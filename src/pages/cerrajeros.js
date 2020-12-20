9306f6244942d4b31de6620c60835275495791d3
Alex <alexgarcia1771@outlook.com>
2020-12-20 13:11:51 +0100

    alex

--- src/pages/cerrajeros.js
+++ src/pages/cerrajeros.js
@@ -11,7 +11,7 @@ class cerrajeros extends Component {
       }
     
       getlockmasters = async () => {
-        const res = await axios.get(`http://localhost:4000/lock/locksmith`);
+        const res = await axios.get(`${process.env.REACT_APP_API_URI}/lock/locksmith`);
         this.setState({ lockmasters: res.data });
         console.log(res.data);
       };
@@ -22,7 +22,7 @@ class cerrajeros extends Component {
     
       deleter = async (id) => {
         try{
-          await axios.delete('http://localhost:4000/lock/locksmith/'+id)
+          await axios.delete(`${process.env.REACT_APP_API_URI}/lock/locksmith/${id}`)
           console.log(id)
         this.getlockmasters()
         }
